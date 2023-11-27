7e551f29ee1715acf9c4830d78a679a6d91d1d34
Muhammad Rabeet <149701207+MuhammadRabeet1@users.noreply.github.com>
2023-11-27 15:48:41 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/routes.dart';
+import 'package:shop_app/screens/splash/splash_screen.dart';
+import 'package:shop_app/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way - Template',
+      theme: AppTheme.lightTheme(context),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class AppTheme {
+  static ThemeData lightTheme(BuildContext context) {
+    return ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      fontFamily: "Muli",
+      appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+          titleTextStyle: TextStyle(color: Colors.black)),
+      textTheme: TextTheme(
+        bodyLarge: TextStyle(color: kTextColor),
+        bodyMedium: TextStyle(color: kTextColor),
+        bodySmall: TextStyle(color: kTextColor),
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        contentPadding: EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        border: outlineInputBorder,
+      ),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
+  }
+}
+
+const OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(28)),
+  borderSide: BorderSide(color: kTextColor),
+  gapPadding: 10,
+);
